d6d775f2da7311bc0d5e4292a116b0f5824ae748
segredo-luciano <lucianosegredo20@gmail.com>
2023-10-16 16:58:16 -0300

    returning users by dto

--- src/main/java/com/segredo/webservicemongodb/resources/UserResource.java
+++ src/main/java/com/segredo/webservicemongodb/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.segredo.webservicemongodb.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.segredo.webservicemongodb.domain.User;
+import com.segredo.webservicemongodb.dto.UserDTO;
 import com.segredo.webservicemongodb.services.UserService;
 
 @RestController
@@ -19,9 +21,10 @@ public class UserResource {
 	private UserService userService;
 	
 	@GetMapping
-	public ResponseEntity<List<User>> findAll() {		
+	public ResponseEntity<List<UserDTO>> findAll() {		
 		List<User> list = userService.findAll();
+		List<UserDTO> listDTO = list.stream().map(u -> new UserDTO(u)).collect(Collectors.toList());
 		
-		return ResponseEntity.ok().body(list);
+		return ResponseEntity.ok().body(listDTO);
 	}
 }
