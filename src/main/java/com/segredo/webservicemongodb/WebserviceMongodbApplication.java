b6f21ec16e526bd5d06c9648b1b8722dd2a5ddd7
segredo-luciano <lucianosegredo20@gmail.com>
2023-10-16 15:10:06 -0300

    initializing project

---
+++ src/main/java/com/segredo/webservicemongodb/WebserviceMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.segredo.webservicemongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebserviceMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebserviceMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/segredo/webservicemongodb/WebserviceMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.segredo.webservicemongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebserviceMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
